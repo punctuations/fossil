2ec2150cb801d4fc4624f09dbd17044925012233
Matt <mattsnaccc@gmail.com>
2026-06-30 03:55:09 -0700

    fix: Astro redirect

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,3 +1,7 @@
 import { defineConfig } from "astro/config";
 
-export default defineConfig({});
+export default defineConfig({
+  redirects: {
+    "/examples": "https://github.com/punctuations/fossil/blob/main/examples/README.md#lossy"
+  }
+});
